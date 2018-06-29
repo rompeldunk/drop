0befc6e85fcb3e7ad1111a1f7a4f34f99509f31a
rompeldunk <magnus.gule@gmail.com>
2018-06-29 22:09:07 +0200

    auto find absolute path

--- config.php
+++ config.php
@@ -1,13 +1,11 @@
-<?php
+    <?php
 
 /* Configuration file */
 
-$abs_path = realpath(dirname(__FILE__));
-echo $abs_path;
+$target_dir = "upload/";                                    // Relative path to upload folder
+$debug = false;                                             // Enables output when running on cronjob.php from browser
+$dir = realpath(dirname(__FILE__)) ."/" . $target_dir;      // Absolute path to root folder
 
-$target_dir = "upload/";            // Relative path to upload folder
-$dir = "/var/www/drop/upload/";     // Absolute path to upload folder
-$debug = false;                     // Enables output when running on cronjob.php from browser
 
 $timeout = 259200;                  // Default: 259200 seconds = 72 hours
 
