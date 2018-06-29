75ee2df8fa15aadfe1ef246a9e79b338ca5f9f18
rompeldunk <magnus.gule@gmail.com>
2018-06-29 22:10:18 +0200

    prettify

--- config.php
+++ config.php
@@ -4,10 +4,15 @@
 
 $target_dir = "upload/";                                    // Relative path to upload folder
 $debug = false;                                             // Enables output when running on cronjob.php from browser
+$timeout = 259200;                                          // Default: 259200 seconds = 72 hours
+
+
+/* Check upload/resources/config.php for options in DirectoryLister */
+
+
 $dir = realpath(dirname(__FILE__)) ."/" . $target_dir;      // Absolute path to root folder
 
 
-$timeout = 259200;                  // Default: 259200 seconds = 72 hours
 
-/* Check upload/resources/config.php for options in DirectoryLister */
+
 
